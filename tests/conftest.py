ed5de772f669d01e43061272305d84662248d5c5
Albina <albizuewa@gmail.com>
2024-10-20 22:33:43 -0700

    Add project content

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+
+from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture()
+def setup_browser():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+    yield browser
